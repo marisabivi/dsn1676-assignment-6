de1e985d8aa8d9a54c5e926871bfc407d1632349
marisabivi <biv.marisa@yahoo.com>
2015-03-11 14:33:35 -0400

    VARs added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $showHide = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+var $btnCollapse = $('.btn-collapse-expand');
+var $panel = $('.circle');
+
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
