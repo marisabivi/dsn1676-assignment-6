e6dd573b9794c8bee0b8f4d718902290a6e6b881
marisabivi <biv.marisa@yahoo.com>
2015-03-11 15:59:07 -0400

    var missing for bounce, keyframes added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var $diamond = $('.diamond');
 var $btnCollapse = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+
 var $btnAppend = $('.btn-append');
 var $list = $('.list');
 
